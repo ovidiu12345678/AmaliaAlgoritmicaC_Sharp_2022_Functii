fc8c6d3e7a431933ccdb966e842e9da5536ae2cc
OVY <OVY@DESKTOP-Q2TVGH5>
2022-12-18 23:05:42 +0200

    Add project files.

---
+++ AmaliaAlgoritmicaC_Sharp_2022_Functii/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+namespace Algoritmica_Functii
+{
+    class Program
+    {
+        ////static void Main(string[] args)
+        ////{
+        ////    int suma, penultimulNumar;
+        ////    Console.WriteLine("Introduceti numerele dorite pana la aparitia unor doua numere n consecutive !");
+        ////    int numar = int.Parse(Console.ReadLine());
+
+        ////    suma = numar;
+        ////    penultimulNumar = numar;
+
+        ////    numar = int.Parse((Console.ReadLine()));
+
+        ////    suma += numar;
+
+        ////    while(numar != penultimulNumar)
+        ////    {
+        ////        penultimulNumar = numar;
+        ////        numar = int.Parse(Console.ReadLine());
+
+        ////        suma += numar;
+        ////    }
+
+        ////    Console.WriteLine("Suma numerelor introduse este " + suma);
+        ////}
+
+
+
+        //static void Main(string[] args)
+        //{
+        //    int suma = 0, penultimulNumar = 0, numar = 0;
+
+
+
+        //    AfiseazaMesajText();
+        //    Console.WriteLine("Numerele dorite  suunt ");
+
+        //    suma = int.Parse(Console.ReadLine());
+        //    penultimulNumar = int.Parse(Console.ReadLine());
+
+        //    CalculSumaNumereConsecutive(suma, penultimulNumar, numar);
+
+        //}
+
+
+        //static void  AfiseazaMesajText()
+        //{
+
+        //    Console.WriteLine( "Introduceti valorile dorite pana la aparitia celor doua numere consecutive !");
+
+        //}
+
+
+        //static void CalculSumaNumereConsecutive(int suma, int penultimulNumar, int numar)
+        //{
+        //    suma = numar;
+        //    penultimulNumar = numar;
+
+        //    suma += numar;
+
+        //       while(numar != penultimulNumar)
+        //       {
+        //           penultimulNumar = numar;
+        //           numar = int.Parse(Console.ReadLine());
+
+        //           suma += numar;
+        //       }
+
+
+        //}
+
+    }
+}
